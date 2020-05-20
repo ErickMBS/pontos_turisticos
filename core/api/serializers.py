f0edfa6a936155567d987cd90b6c587af9236461
Erick_braga <erickmth.braga@gmail.com>
2020-05-20 14:24:22 -0300

    antes do deploy do heroku

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,34 @@
+from comentarios.api.serializers import ComentarioSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
+from atracoes.api.serializers import AtracaoSerializer
 from core.models import PontoTuristico
 
+
 class PontoTuristicoSerializer(ModelSerializer):
+    comentarios = ComentarioSerializer(many=True)
+    avaliacoes = AvaliacaoSerializer(many=True)
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+    descricao_completa = SerializerMethodField()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+        fields = (
+            'id',
+            'nome',
+            'descricao',
+            'aprovado',
+            'foto',
+            'atracoes',
+            'comentarios',
+            'endereco',
+            'avaliacoes',
+            'descricao_completa',
+            'descricao_completa2',
+        )
+
+    def get_descricao_completa(self, obj):
+        return f'{obj.nome} - {obj.descricao}'
